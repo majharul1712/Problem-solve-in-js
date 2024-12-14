67d29c22c7e8ea0cf9b2611001464e3383015cba
M MAJHARUL <majharulmunna01@gmail.com>
2024-12-14 11:31:28 +0600

    Upload 18-20 file problem slove

---
+++ Easy problem/19p.js
@@ -0,0 +1,7 @@
+// Write a program to print all odd numbers from 1 to 100.
+for (let i = 1; i <= 100 ; i++) {
+    if (i % 2 !== 0) {
+        console.log(i)
+    }
+    
+}
---
+++ Easy problem/20p.js
@@ -0,0 +1,6 @@
+// Write a function that returns the sum of two numbers.
+function sum(a, b) {
+    return a + b
+}
+let result = sum(6,10)
+console.log("This is sum",result)
